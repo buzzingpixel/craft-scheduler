5035287dcdfd5aa329ca8a587f85534b12a0757e
TJ Draper <tj@buzzingpixel.com>
2021-09-28 08:17:41 -0500

    Initialize project

---
+++ src/CraftSchedulerPlugin.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BuzzingPixel\CraftScheduler;
+
+use craft\base\Plugin;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+class CraftSchedulerPlugin extends Plugin
+{
+}
